7102949c863c9099d8073ac1ee3e988db8cd937a
Mahesh <itstribe.mahi@gmail.com>
2022-08-30 16:39:04 +0530

    arrow function

---
+++ arrowFunction.js
@@ -0,0 +1,78 @@
+// // basic arroe function
+// let str=(x)=> x*3;
+// console.log(str(10));
+
+// word length
+// let mahi=(s)=> s.toUpperCase();
+// console.log(mahi('Mahesh gh ')) 
+
+// without passing parameter
+// let call=()=>"CGCS";
+// console.log(call());
+
+
+// using rest in arrow function
+//let res=(num, a,...val)=>val; // her we can use any word instead of val but we have to use same word after the arrow function.
+// let res=(num, ...val, a) // gives error. We should mention rest operator at last.
+//console.log(res(12,34,45,23,23,2,3,123));
+
+
+
+
+
+// const numbers = [65, 44, 12, 4];
+// const newArr = numbers.map(myFunction);
+
+// function myFunction(num) {
+//   return num * 10;aaaa
+// }
+// console.log(newArr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
